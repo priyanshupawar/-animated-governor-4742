8d7d7fcd1bc1139d7604c34401bd7309e859ad37
priyanshupawar <talk20priyanshu@gmail.com>
2022-09-30 07:24:50 +0530

    wishlist

---
+++ wishlist.js
@@ -0,0 +1,45 @@
+let dataLS = JSON.parse(localStorage.getItem("wishlist"));
+
+const display = (dataLS) => {
+  if (dataLS === null) {
+    let container = document.getElementById("conatiner-product");
+    container.innerHTML = null;
+    let p = document.createElement("p");
+    p.innerText = "Save your favorite items to start building your wishlist";
+    let btn = document.createElement("button");
+    btn.innerText = "Get Inspired";
+
+    container.append(p, btn);
+  } else {
+    let container_count = document.getElementById("count-data");
+
+    let count = document.createElement("p");
+    count.innerText = dataLS.length + "PIECES";
+    container_count.append(count);
+    let container = document.getElementById("conatiner-product");
+    container.innerHTML = null;
+    dataLS.forEach(function (ele) {
+      let div = document.createElement("div");
+      let img = document.createElement("img");
+      img.src = ele.image;
+      let type = document.createElement("h3");
+      type.innerText = ele.type;
+      let name = document.createElement("p");
+      name.innerText = ele.name;
+      let price = document.createElement("p");
+      price.innerText = ele.price;
+      let btn1 = document.createElement("button");
+      btn1.innerText = "Add to bag";
+      btn1.addEventListener("click", () => {
+        addToCart(ele);
+      });
+      div.append(img, type, name, price, btn);
+      container.append(div);
+    });
+  }
+};
+
+display(dataLS);
+const addToCart = (ele) => {
+  localStorage.setItem("Add_to_cart", JSON.stringify(ele));
+};
